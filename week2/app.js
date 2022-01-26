af7584b012d06bbc4f67fafafaa9c5855c7d4224
jordandubreuil <jdubreuil@neit.edu>
2022-01-26 12:50:31 -0500

    End Of Class 1/26

--- week2/app.js
+++ week2/app.js
@@ -4,6 +4,11 @@ var path = require('path')
 var bodyparser = require('body-parser')
 var mongoose = require('mongoose')
 
+//setup body-parser middleware to use in application
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(express.json())
+
 //setup connection to mongoose
 mongoose.connect('mongodb://localhost:27017/gameEntries',{
     useNewURLParser:true
@@ -13,13 +18,44 @@ mongoose.connect('mongodb://localhost:27017/gameEntries',{
     console.log(err)
 })
 
+//Use Models for Database
+require('./models/Game')
+var Game = mongoose.model('game')
+
 //basic code for saving an entry
 //model of data
-var Game = mongoose.model('Game', {nameofGame:String})
+/*var Game = mongoose.model('Game', {nameofGame:String})
 
 var game = new Game({nameofGame:"Skyrim"})
 game.save().then(function(){
     console.log("Game Saved")
+})*/
+
+
+//Setup the post route to save the data
+app.post('/saveGame', function(req,res){
+    console.log("Request Made")
+    console.log(req.body)
+    //save the data
+    new Game(req.body).save().then(function(){
+        res.redirect('gamelist.html')
+    })
+})
+
+//setup get route to send data to Game List
+app.get('/getData', function(req,res){
+    Game.find({}).then(function(game){
+        console.log("Retrieving Games")
+        console.log({game})
+        res.json({game})
+    })
+})
+
+//setup post route to delete game entry
+app.post('/deleteGame', function(req,res){
+    console.log("Game Deleted" , req.body._id)
+    Game.findByIdAndDelete(req.body._id).exec()
+    res.redirect('gamelist.html')
 })
 
 //sets up static server and sets up connection
